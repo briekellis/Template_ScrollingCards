fbcbb12223686091e63a5b8156aa83996dba0c4a
Brie Ellis <briekellis@gmail.com>
2020-01-11 12:37:24 -0800

    Initial Commit

---
+++ Temp1/Temp1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Temp1.MvcApplication" Language="C#" %>
---
+++ Temp1/Temp1/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Temp1.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string ProductTitle { get; set; }
+        public string ProductDescription { get; set; }
+        public double ProductCost { get; set; }
+    }
+}
